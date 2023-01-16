24fa33fcee7862ccd2f1c44e848849db695b3dd3
Chudinov Ivan <ikchudinov@gmail.com>
2023-01-16 15:49:03 +0300

    Расширение функционала - учет даты забора
    * Добавлен ключ `dates_switch` для учета даты забора образца:
      * Добавлена функция для перевода даты из имени последовательности
        в формат np.datetime64;
      * Добавлен класс `DatesFilterException` для обработки ситуаций,
        когда не найдено последовательностей для починки по результатам
        учета даты;
      * Расширен функционал цикла починки при запуске с учетом дат,
        не внесено изменений в предыдущий механизм работы;
    * Добавлен ключ `row_length` для возможности редактировать длину
      строки в output Fasta файлах.

--- fix_seq.py
+++ fix_seq.py
@@ -18,8 +18,6 @@ from tqdm.auto import tqdm
 from loguru import logger
 
 
-# TODO: добавить конвертацию fasta-файлов с записью их в /tmp
-# TODO: добавить try-except проверку наличия файла из df в файлах fasta, если нет, то warning
 def setup_args():
     parser = argparse.ArgumentParser(description="The fixing if the sequences according to the " +
                                                  "distances matrix",
@@ -57,7 +55,57 @@ def setup_args():
                         type=str, required=False,
                         help="Path for the reference to be used")
 
+    parser.add_argument('--dates_switch',
+                        action='store_false', required=False,
+                        help="Account for the dates from the sequence naming for the fixing process. \n" +
+                             "Turns `False` if passed, no dates correction would be provided.")
+
+    # чтобы не вводить файл настроек, но настраивать эту ерунду хочется быстро и легко
+    parser.add_argument('--row_length',
+                        type=int, required=False,
+                        default=60, help="Style parameter for the length per row in output Fasta(s)")
+
     return parser.parse_args()
+
+
+def create_date_f_name(seq_name):
+    """
+    Извлечение даты забора образца из полного наименования. Предполагается, что дата забора записана после слэша.
+    В случае, если даты не найдется, то в качестве даты забора выступит абстрактно большая дата.
+    Следует обратить внимание, что в качестве найденной даты будет использоваться первая найденная дата, т.е. если
+    в наименование входят две даты последовательности, то будет использована первая из них. \n \n
+    Случаи обработки обнаруженной даты по паттерну записи после слэша:
+     1) Если дата нормальная -- 2021-07-12, то она и будет обработана в таком виде;
+     2) Если дата урезана до месяца -- 2021-07, то в массив пойдет 2021-08-01;
+     3) Если дата урезана до года -- 2021, то в массив пойдет 2021-12-31. \n \n
+    :param seq_name: полное наименование последовательности;
+    :return: объект даты.
+    """
+    to_ret = "2030-12-31"  # абстрактная большая дата
+    pat = re.search(r"/\d{4}-\d{2}-\d{2}", seq_name)  # сперва ищем нормальную запись даты
+    if pat:  # если находим, то обрабатываем её и готовим на возврат
+        to_ret = np.datetime64(pat.group()[1:])
+    else:  # если не находим, снижаем критерии до поиска месяца
+        pat = re.search(r"/\d{4}-\d{2}", seq_name)
+        if pat:  # если находим, то округляем до первого числа следующего месяца
+            to_ret = np.datetime64(pat.group()[1:]) + np.timedelta64(1, 'M') + np.timedelta64(0, 'D')
+        else:  # если не находим, вновь снижаем критерии к поиску
+            pat = re.search(r"/\d{4}", seq_name)
+            if pat:  # если находим, то округляем до 31 декабря этого года
+                to_ret = np.datetime64(pat.group()[1:], 'Y') + np.timedelta64(11, 'M') + np.timedelta64(30, 'D')
+            # если не находим ни одной даты по паттерну, то вернется изначальная абстрактно большая дата
+    return to_ret
+
+
+class DateFilterException(Exception):
+    """
+    Кастомный класс Exception для обработки ситуации, когда учет дат привел к отсутствию
+    подходящих последовательностей для починки, а вмешиваться в общий алгоритм перебора последовательностей пагубно.
+    """
+    def __init__(self, seq_name, seq_date):
+        self.name = seq_name
+        self.date = seq_date
+        super.__init__(self.name, self.date)
 
 
 def all_equal(iterable):
@@ -70,7 +118,7 @@ def all_equal(iterable):
     return next(g, True) and not next(g, False)
 
 
-def filter_only(record):
+def filter_only(record):  # запись логов процесса починки
     return record["level"].name == "TRACE"
 
 
@@ -84,6 +132,16 @@ def main():
         rows_names = fr.read().splitlines()
     with open(args.input_p + "_columns.txt", "r") as fr:
         cols_names = fr.read().splitlines()
+    
+    # создаем списки дат для осуществления обоснованного выбора
+    # вот это объявляем, чтобы даже если что-то пошло не так, просто вылетали ошибки отсутствия метода, а не NameSpace
+    rows_dates, cols_dates = (None, None)
+    # если все же ключ будет передан, здесь будет False и не запустится обработка дат
+    if args.dates_switch:
+        logger.debug("Fixing process will account for the dates from sequence names")
+        logger.debug("Parsing dates")
+        rows_dates = np.array([create_date_f_name(x) for x in rows_names])
+        cols_dates = np.array([create_date_f_name(x) for x in cols_names])
 
     to_fix_dict = dict()
     fixing_dict = dict()
@@ -176,7 +234,29 @@ def main():
             # находим имена всех последовательностей с этим минимальным значением
             asv = df.loc[target][lambda x: x == min_value].index
             logger.trace(f">>{target}; score {min_value}; neighbours {len(asv)}")
-            if len(asv) > 10:  # временная заглушка для большого количества ближайших
+            if args.dates_switch:
+                # получаем целевую дату по индексу целевого имени последовательности
+                target_date = rows_dates[df.index.get_loc(target)]
+                # получаем даты по индексам имен вдоль массива наименований
+                fixing_dates = cols_dates[df.columns.get_indexer(asv)]
+                # фильтруем массив наименований в соответствии с примененным условием на значение даты
+                dates_condition = np.where(fixing_dates < target_date)
+                updated_asv = asv[dates_condition]
+                if len(updated_asv) == 0:  # если получилось, что длинна массива ноль
+                    # и выходим из итерации, эту последовательность мы не чиним
+                    raise DateFilterException(target, target_date)
+                elif len(updated_asv) > 10:
+                    # если дат больше 10, то мы заранее и гарантированно обрезаем число хитов для починки,
+                    # чтобы при дальнейшей обработке этого точно не произошло,
+                    # что позволяет не менять функционал программы при ключе `dates_switch` False
+                    asv = updated_asv[np.argsort(fixing_dates[dates_condition])[:10]]
+                else:
+                    # если записей не ноль и не более 10, то передаем как есть
+                    asv = updated_asv
+            # даже если ключ `dates_switch` передан, функционал работы починки не изменен
+            # если мы не учитываем даты при починке, то мы просто выбираем 10 случайных последовательностей,
+            # когда пул для починки очень большой
+            if len(asv) > 10:
                 asv = np.random.choice(asv, 10)
             logger.trace(";".join(asv))
             # пишем в файл во временной директории референс, целевую последовательность, ближайшие последовательности
@@ -201,10 +281,10 @@ def main():
             search_ali_end = re.search(r"[a-z]-+$", current_proceedings[reference_name])
             # получаем переменные для хранения начала и конца кодирующих частей
             wr_lb, wr_rb = search_ali_start.end() - 1, search_ali_end.start() + 1
-            # ищем [nN]+ в кодирующей части целевой последовательности
+            # ищем [nN]+ или иные неоднозначно определенные нуклеотиды в кодирующей части целевой последовательности
             found_unc_s = [(m.start(0),
-                           m.end(0)) for m in re.finditer("[ryswkmbdhvnRYSWKMBDHVN]+",
-                                                          current_proceedings[target][wr_lb:wr_rb])]
+                            m.end(0)) for m in re.finditer("[ryswkmbdhvnRYSWKMBDHVN]+",
+                                                           current_proceedings[target][wr_lb:wr_rb])]
             # производим замену выбранных интервалов
             resulting_seq_beginning = current_proceedings[target][:wr_lb]  # кусок, который не чиним
             under_construction = [_ for _ in current_proceedings[target][wr_lb:wr_rb]]  # кусок, который чиним
@@ -230,16 +310,26 @@ def main():
 
             # фиксируем созданную исправленную часть
             resulting_seq = resulting_seq_beginning + "".join(under_construction) + resulting_seq_ending
-            res_seq_to_wr = "\n".join(textwrap.wrap(resulting_seq.replace("-", "").upper(), 60))
-
+            res_seq_to_wr = "\n".join(textwrap.wrap(resulting_seq.replace("-", "").upper(), args.row_length))
+            # записываем в файл исправленных с учетом успеха починки всех интервалов
             if fixed_intervals == len(found_unc_s):
                 out_fwr.write(f">{target}|{fixed_intervals}/{len(found_unc_s)}|\n" + res_seq_to_wr + "\n")
             else:
                 out_not_fwr.write(f">{target}|{fixed_intervals}/{len(found_unc_s)}|\n" + res_seq_to_wr + "\n")
+        except DateFilterException as e:  # значит не удалось пройти фильтр дат и починить
+            # оповещаем об этом в логах процесса починки
+            logger.trace(f"No fixer found for the {e.name} accounting for the date {e.date}")
+            # пишем последовательность as is в файл неисправленных
+            seq_for_wr = "\n".join(textwrap.wrap(linecache.getline(to_fix_fasta_path, to_fix_dict[target]),
+                                                 args.row_length))
+            out_not_fwr.write(f">{target}|-1/0|\n" + seq_for_wr + "\n")
         except (Exception, ) as e:
+            # если любая другая ошибка возникла, оповещаем уже глобально
             logger.warning(f"Unexpected error with {target}")
             logger.error(e)
-            seq_for_wr = "\n".join(textwrap.wrap(linecache.getline(to_fix_fasta_path, to_fix_dict[target]), 60))
+            # тем не менее пишем последовательность as is в файл неисправленных
+            seq_for_wr = "\n".join(textwrap.wrap(linecache.getline(to_fix_fasta_path, to_fix_dict[target]),
+                                                 args.row_length))
             out_not_fwr.write(f">{target}|-1/0|\n" + seq_for_wr + "\n")
 
     # закрываем файлы после записи
